3467af3dc1954b42f00870b6fa90fe20eec1a3ed
yexuansong <yexuan_song@sfu.ca>
2025-10-17 13:57:46 -0700

    adding code that calculate the number of transition events missing after downsampling the tree

---
+++ lost_info.R
@@ -0,0 +1,48 @@
+get_trans_hist <- function(true_tree_info){
+  n_tips <- length(true_tree_info$tip.label)
+  n_nodes <- true_tree_info$Nnode
+  n_total <- n_tips + n_nodes
+  
+  states <- rep(NA, n_total)
+  
+  for (i in 1:n_tips) {
+    tip_name <- true_tree_info$tip.label[i]
+    states[i] <- true_info$State[true_info$label == tip_name]
+  }
+  
+  for (i in 1:n_nodes) {
+    node_num <- n_tips + i
+    if (!is.null(true_tree_info$node.label) && length(true_tree_info$node.label) >= i) {
+      node_name <- true_tree_info$node.label[i]
+      states[node_num] <- true_info$State[true_info$label == node_name]
+    }
+  }
+  
+  transitions <- data.frame(
+    parent_node = true_tree_info$edge[, 1],
+    child_node = true_tree_info$edge[, 2],
+    parent_state = states[true_tree_info$edge[, 1]],
+    child_state = states[true_tree_info$edge[, 2]]
+  )
+  
+  
+  transition_counts <- list(
+    state1_to_state2 = sum(transitions$parent_state == 1 & 
+                             transitions$child_state == 2, na.rm = TRUE),
+    state2_to_state1 = sum(transitions$parent_state == 2 & 
+                             transitions$child_state == 1, na.rm = TRUE),
+    state1_to_state1 = sum(transitions$parent_state == 1 & 
+                             transitions$child_state == 1, na.rm = TRUE),
+    state2_to_state2 = sum(transitions$parent_state == 2 & 
+                             transitions$child_state == 2, na.rm = TRUE)
+  )
+  return(transition_counts$state1_to_state2+transition_counts$state2_to_state1+
+           transition_counts$state1_to_state1 + transition_counts$state2_to_state2)
+  
+}
+
+get_diff_hist <- function(tree1,tree2){
+  return(abs(get_trans_hist(tree1) - get_trans_hist(tree2)))
+}
+
+
